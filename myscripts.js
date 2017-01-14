75e3a43a96b313f81e0baabbac1b22053c8c2814
pjkim02 <pjkim02@gmail.com>
2017-01-14 13:11:44 -0500

    readded button

---
+++ myscripts.js
@@ -0,0 +1,5 @@
+function changeColor(){
+  document.getElementById("intro").style.color="red"
+  document.getElementById("content").style.color="white"
+  document.getElementById("aboutme").style.color="white"
+}

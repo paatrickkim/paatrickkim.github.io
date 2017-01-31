1d159a14e18c7ca49f85c5813bb9ab29445806fe
pjkim02 <pjkim02@gmail.com>
2017-01-30 22:19:37 -0500

    flashcard game

--- myscripts.js
+++ myscripts.js
@@ -3,3 +3,26 @@ function changeColor(){
   document.getElementById("content").style.color="white"
   document.getElementById("aboutme").style.color="white"
 }
+
+var operand1;
+var operand2;
+
+function getNumbers(){
+  operand1 = Math.floor(Math.random() * 30) + 1;
+  operand2 = Math.floor(Math.random() * 30) + 1;
+  document.getElementById("operand1").innerHTML=operand1;
+  document.getElementById("operand2").innerHTML=operand2;
+  document.getElementById("userAnswer").value="";
+}
+
+function checkAnswer(){
+  var sum = operand1 + operand2;
+  var userSum = document.getElementById("userAnswer").value;
+  
+  if(sum == userSum){
+    document.getElementById("results").innerHTML="You're so smart. Correct!"
+  }
+  else{
+    document.getElementById("results").innerHTML="...incorrect. Check your work!"
+  }
+}

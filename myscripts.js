60b92aacebeb6d95f0c60bd1aa7e30f89bff5faa
pjkim02 <pjkim02@gmail.com>
2017-02-04 13:15:12 -0500

    gallery

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,5 @@
+//this is a random comment, take note.
+
 function changeColor(){
   if(document.getElementById("intro").style.color == "blue"){
     document.getElementById("intro").style.color="black";
@@ -29,3 +31,26 @@ function checkAnswer(){
     document.getElementById("results").innerHTML="Incorrect. Check Your Work!"
   }
 }
+
+var pics = [
+  "https://kidskonnect.com/assets/uploads/2014/12/birds-featured.jpg",
+  "http://weknowyourdreams.com/images/bird/bird-02.jpg",
+  "https://s-media-cache-ak0.pinimg.com/564x/90/c9/82/90c982d2b8118104dc2063373d825627.jpg"
+];
+
+var picIndex = 0;
+
+function goLeft(){
+  if(picIndex > 0){
+    picIndex = picIndex - 1;
+    document.getElementById("pic").src=pics[picIndex];
+  }
+}
+
+function goRight(){
+    if(picIndex < (pic.length - 1)){
+    picIndex = picIndex + 1;
+    document.getElementById("pic").src=pics[picIndex];
+  }
+}
+
